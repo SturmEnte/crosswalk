6e05cfc0ca223ddcccaaa0b456f603151e4ca07d
Jonas <sturmente@proton.me>
2022-10-06 18:15:09 +0200

    Added comments

--- crosswalk.ino
+++ crosswalk.ino
@@ -1,8 +1,3 @@
-/* TBD
-  - Register button presses but execute them when the time is right 
-
-*/
-
 const int BUTTON = A3;
 
 const int PED_GREEN = 2;
@@ -20,41 +15,49 @@ unsigned long nextPress = 0;
 bool pressed = false;
 
 void setup() {
+  // Pin 4 is not used but this looks better and saves time while writing
   for(int i = PED_GREEN; i <= CAR_RED; i++) {
     pinMode(i, OUTPUT);
   }
 
-  // Defualt state
+  // Set the defualt state of the traffic lights
   digitalWrite(CAR_GREEN, HIGH);  
   digitalWrite(PED_RED, HIGH);
 }
 
 void loop() {
+  // Check if the button is pressed and if so, save it in the pressed value
   if(digitalRead(BUTTON) == LOW) {
     pressed = true;
   }
   
+  // Check if the button was pressed and if a green phase for the pedestrians can be started
   if(pressed && nextPress <= millis()) {
     
+    // Set the traffic light of the cars to yellow
     digitalWrite(CAR_GREEN, LOW);
     digitalWrite(CAR_YELLOW, HIGH);
     delay(YELLOW_TIME);
     
+    // Set the traffic light of the cars to red and of the pedestrians to green
     digitalWrite(CAR_YELLOW, LOW);
     digitalWrite(CAR_RED, HIGH);
     digitalWrite(PED_RED, LOW);
     digitalWrite(PED_GREEN, HIGH);
     delay(RED_TIME);
     
+    // Set the traffic light of the cars to yellow-red and of the pedestrians to red
     digitalWrite(CAR_YELLOW, HIGH);
     digitalWrite(PED_GREEN, LOW);
     digitalWrite(PED_RED, HIGH);
     delay(YELLOW_TIME);
     
+    // Set the traffic light of the cars to green
     digitalWrite(CAR_YELLOW, LOW);
     digitalWrite(CAR_RED, LOW);
     digitalWrite(CAR_GREEN, HIGH);
     
+    // Reset the pressed value and calculate the earliest time of the next green phase for the pedestrians
     nextPress = millis() + TIME_BETWEEN_PRESSES;
     pressed = false;
   }
